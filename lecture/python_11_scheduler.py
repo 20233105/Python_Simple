37c31e11cfbfbdfc28c96cf2cf2d9b8b797ab2ad
20233105 <144192048+20233105@users.noreply.github.com>
2023-12-01 13:23:33 +0900

    Add files via upload

--- lecture/python_11_scheduler.py
+++ lecture/python_11_scheduler.py
@@ -1,22 +1,40 @@
 # 스케줄러
-#   - 정해진 시간에 동작하도록 프로그램을 스케줄링
-#   예) 5초마다 한 번씩 동작
-#       특정일에만 동작
-#       하루에 한 번 동작
+#  - 정해진 시간에 동작하도록 프로그램을 스케줄링
+#  예) 5초마다 한번씩 동작
+#      특정일(크리스마스)에만 동작
+#      하루에 한 번 동작
+import time
 
-#   apscheduler
-#   1.blocking
-#   2.background
+# apscheduler
+#  1.blocking
+#  2.background
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 def print_now():
     print(datetime.now())
+
+
+def print_love():
+    print("I LOVE YOU")
+
+
 # 1.스케줄러 생성
-scheduler = BlockingScheduler()
+# scheduler = BlockingScheduler()
+scheduler = BackgroundScheduler()
 # 2.스케줄러 등록(일, 주기, 5초)
-scheduler.add_job(print_now, "interval", seconds=5)
+scheduler.add_job(print_now, "interval", seconds=1)
+scheduler.add_job(print_love, "cron", hour=13, minute=16)
+# - date: 특정일, 특정 날짜에만 동작
+# - interval: 간격 별로(5초, 10분, 1시간 등)
+# - CRON(*):만능(ex:매일 특정 시간에~)
+
 # 3.스케줄러 실행
 scheduler.start()
+
+# 임의로 work
+while True:
+    time.sleep(1)
