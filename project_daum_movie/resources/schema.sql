11e82c18c64858bc9db4789fb25774bbde8367bf
20233105 <144192048+20233105@users.noreply.github.com>
2023-12-06 02:28:35 +0900

    Add files via upload

--- project_daum_movie/resources/schema.sql
+++ project_daum_movie/resources/schema.sql
@@ -1,6 +1,5 @@
 # 다음 영화 리뷰 Table
-CREATE TABLE `tbl_review`
-(
+CREATE TABLE `tbl_review` (
 	`no` INT(10) NOT NULL AUTO_INCREMENT,
 	`title` VARCHAR(100) NOT NULL,
 	`review` VARCHAR(500),
@@ -9,6 +8,6 @@ CREATE TABLE `tbl_review`
 	`reg_date` VARCHAR(50) NOT NULL,
 	PRIMARY KEY (`no`) USING BTREE
 )
-COMMENT= '다음 영화 리뷰'
+COMMENT='다음 영화 리뷰'
 AUTO_INCREMENT=1
 ;
